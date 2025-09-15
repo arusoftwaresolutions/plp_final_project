2f97a768f9ef99757097f9f2a691155930350ec5
Araya Haftu <arayahaftu1229@gmail.com>
2025-09-15 16:35:09 +0300

    update requirments

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,4 @@
-try:
-    from pydantic_settings import BaseSettings
-except ImportError:
-    from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 from typing import Optional
 
 class Settings(BaseSettings):
